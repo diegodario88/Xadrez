a0c8cc3e49cac6ad20582486bf96e211d2cb2e2d
Diego Dario <diegodario88@gmail.com>
2019-04-29 08:16:31 -0300

    Posicao Xadrez

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -8,19 +8,10 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
-                tab.ColocarPeca(new Torre(Cor.Preto, tab), new Posicao(0, 0));
-                tab.ColocarPeca(new Torre(Cor.Preto, tab), new Posicao(1, 3));
-                tab.ColocarPeca(new Rei(Cor.Preto, tab), new Posicao(3, 6));
-                Tela.ImprimirTabuleiro(tab);
-                Console.ReadLine();
-            }
-            catch(TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            Console.WriteLine(pos.ToPosicao());
+
+                        
         }
     }
 }
